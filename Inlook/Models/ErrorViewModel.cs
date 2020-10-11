b8baff1ba64e1640208c6ebd8b42811ff059618e
Artur Chmura <01142158@pw.edu.pl>
2020-10-11 12:41:00 +0200

    init

---
+++ Inlook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inlook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
