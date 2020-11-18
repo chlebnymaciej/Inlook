9538cc14391afc6d2528ac535653fde27e3375a5
Artur Chmura <achmura66@gmail.com>
2020-11-18 20:19:53 +0100

    cleanup 2.0

--- Inlook_Core/Models/PostGroupModel.cs
+++ Inlook_Core/Models/PostGroupModel.cs
@@ -8,7 +8,8 @@ namespace Inlook_Core.Models
     public class PostGroupModel
     {
         [Required]
-        [MinLength(3,ErrorMessage = "Group name has to be at least 3 characters long")]
+        [MinLength(3,ErrorMessage = "Group name must contain at least 3 characters")]
+        [MaxLength(40, ErrorMessage = "Group name must be equal to or less than 40 characters")]
         public string  Name { get; set; }
         [MinLength(1)]
         public string[] Users { get; set; }
--- Inlook_Core/Models/UpdateGroupModel.cs
+++ Inlook_Core/Models/UpdateGroupModel.cs
@@ -9,8 +9,8 @@ namespace Inlook_Core.Models
     {
         [Required]
         public string Id { get; set; }
-        [Required]
-        [MinLength(3, ErrorMessage = "Group name has to be at least 3 characters long")]
+        [MinLength(3, ErrorMessage = "Group name must contain at least 3 characters")]
+        [MaxLength(40, ErrorMessage = "Group name must be equal to or less than 40 characters")]
         public string Name { get; set; }
         [MinLength(1)]
         public string[] Users { get; set; }
