6e2a3fcd0b74d0f76aad6d16abfcd8d475de13a4
Maciej Chlebny <chlebny59@gmail.com>
2020-11-30 15:15:54 +0100

    autoamtic deploy test

--- inlook_app/src/Api/urls.ts
+++ inlook_app/src/Api/urls.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://localhost:5001/"
+export const BASE_URL = "https://inlookapi.azurewebsites.net/";
+//"https://localhost:5001/";
+export const BASE_SELF_URL_HTTP = "https://inlook23.azurewebsites.net/";
+//"http://localhost:3000/";
--- inlook_app/src/Authorization/userManager.ts
+++ inlook_app/src/Authorization/userManager.ts
@@ -1,12 +1,14 @@
 import * as OIDC from "oidc-client";
+import { BASE_SELF_URL_HTTP } from "../Api/urls";
+
 
 const config : OIDC.UserManagerSettings = {
 
     authority: "https://login.microsoftonline.com/4a4f96a2-c3a1-4509-9e0e-d3af301e0196/",
     client_id: "48fe1a17-69e2-4942-ab1d-4e288783860f",
-    redirect_uri: "http://localhost:3000/callback",
-    post_logout_redirect_uri: "http://localhost:3000/callback",
-    silent_redirect_uri: "http://localhost:3000/callback",
+    redirect_uri: BASE_SELF_URL_HTTP + "callback",
+    post_logout_redirect_uri: BASE_SELF_URL_HTTP + "callback",
+    silent_redirect_uri: BASE_SELF_URL_HTTP + "callback",
     automaticSilentRenew: true,
 };
 
