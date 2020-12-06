eed941269f0fd3dedcd2cb99f8286ab05f446586
Maciej Chlebny <01142157.sspw@pw.edu.pl>
2020-12-06 12:06:05 +0000

    Updated urls.ts

--- inlook_app/src/Api/urls.ts
+++ inlook_app/src/Api/urls.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://inlookapi.azurewebsites.net/";
 //"https://localhost:5001/";
-export const BASE_SELF_URL_HTTP = "http://localhost:3000/";
+export const BASE_SELF_URL_HTTP = "https://inlook23.azurewebsites.net/";
