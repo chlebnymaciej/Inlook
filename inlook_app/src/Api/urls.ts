3a6a8574c97e606fedc7119fb6c8d89561bcd2ff
Maciej Chlebny <chlebny59@gmail.com>
2020-12-07 11:22:35 +0100

    fix

--- inlook_app/src/Api/urls.ts
+++ inlook_app/src/Api/urls.ts
@@ -1,4 +1,2 @@
-export const BASE_URL = "https://localhost:5001/";
-//"https://inlookapi.azurewebsites.net/";
+export const BASE_URL = "https://inlookapi.azurewebsites.net/";
 //"https://localhost:5001/";
-export const BASE_SELF_URL_HTTP = "https://inlook23.azurewebsites.net/";
