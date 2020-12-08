4dd92de13b2a0b7bb6cb7da0cd0acc9b9d610941
Maciej Chlebny <chlebny59@gmail.com>
2020-12-08 12:25:13 +0100

    unnecesary usings

--- Inlook_Infrastructure/Configurations/UserRoleConfiguration.cs
+++ Inlook_Infrastructure/Configurations/UserRoleConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using Inlook_Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Inlook_Infrastructure.Configurations
 {
