4bda02cd5ab90184315ae0769b6ef9987ed0e47c
Artur Chmura <01142158@pw.edu.pl>
2021-01-01 23:04:35 +0100

    service unit test v1

---
+++ XUnitTests/Services/AttachmentServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests.Services
+{
+    public partial class ServiceTests
+    {
+        [Fact]
+        // All in one, becouse I work on the same attachment
+        public async Task UploadAndGetAndDeleteFileTestAsync()
+        {
+            Stream stream = Stream.Null;
+            string fileName = "test file";
+            var attachment = await attachmentService.UploadFile(stream, fileName);
+            Assert.Equal(fileName, attachment.ClientFileName);
+
+            var file = await attachmentService.GetFile(attachment.Id);
+            Assert.Equal(fileName, file.ClientFileName);
+
+            await attachmentService.DeleteAttachment(attachment.Id);
+            var deletedFile = await attachmentService.GetFile(attachment.Id);
+            Assert.True(deletedFile == null);
+
+        }
+
+    }
+}
---
+++ XUnitTests/Services/GroupServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Inlook_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests.Services
+{
+    public partial class ServiceTests
+    {
+
+        [Fact]
+        // All in one, becouse I work on the same attachment
+        public async Task GroupTest()
+        {
+            string groupName = "TestGroup";
+            var groupModel = new PostGroupModel()
+            {
+                Name = groupName,
+                Users = new string[] {userId.ToString()},
+            };
+            groupService.AddGroup(groupModel, userId);
+
+            var groups = groupService.GetAllGroups(userId);
+            var group = groups.Where(g => g.Name == groupName).FirstOrDefault();
+            Assert.NotNull(group);
+
+            string editedName = groupName + "Edited";
+            var updateGroupModel = new UpdateGroupModel()
+            {
+                Id = group.Id.ToString(),
+                Name = editedName,
+                Users = new string[0] ,
+            };
+            groupService.UpdateGroup(updateGroupModel, userId);
+
+            groups = groupService.GetAllGroups(userId);
+            group = groups.Where(g => g.Name == editedName).FirstOrDefault();
+            Assert.NotNull(group);
+            Assert.True(group.Name == editedName);
+            Assert.True(group.UserGroups.Count == 0);
+
+        }
+
+    }
+}
